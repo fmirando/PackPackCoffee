cfb5c2c7204aefe9a825556f344545d20cce2009
Frank <mirandofrank@gmail.com>
2023-12-04 16:38:51 -0800

    Added appropriate folders for the different components

---
+++ CLIENT/src/components/Contact/Contact.jsx
@@ -0,0 +1,1 @@
+// This component will contain a contact form for clients
---
+++ CLIENT/src/components/PackagesMenu/Packages.jsx
@@ -0,0 +1,2 @@
+// This component will display the different pacakges that Lauren offers
+// Will also contain a contact form for quotes
