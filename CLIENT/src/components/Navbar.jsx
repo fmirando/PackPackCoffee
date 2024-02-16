881d1875d8f28eb32714957093fbdfb0e1f7efee
Frank <mirandofrank@gmail.com>
2024-02-15 16:41:40 -0800

    Installed React Router and created component file and boiler plate code for Navbar

---
+++ CLIENT/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+// Navbar component that will remain sticky on top of every page
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <nav>
+      Navbar
+    </nav>
+  );
+}
