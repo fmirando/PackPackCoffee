d7fce7705a66f963e26e44d3f1346fc9dd4e524a
Frank <mirandofrank@gmail.com>
2024-02-15 17:14:35 -0800

    Added links to each page in Navbar component

--- CLIENT/src/components/Navbar.jsx
+++ CLIENT/src/components/Navbar.jsx
@@ -2,11 +2,32 @@
 // Navbar should contain PackPack logo/name, and the following tabs: About, Contact, Gallery, and Packages
 // Note that "Landing" component should not be included as a tab but rather the user can click on the name/logo and will be directed to landing page
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 export default function Navbar() {
   return (
     <nav>
-      Navbar
+      <Link to="/" className="title">PackPack Coffee</Link>
+      <div>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      <ul>
+        <li>
+          <NavLink to="/about">About</NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact">Contact</NavLink>
+        </li>
+        <li>
+          <NavLink to="/gallery">Gallery</NavLink>
+        </li>
+        <li>
+          <NavLink to="/packages">Packages</NavLink>
+        </li>
+      </ul>
     </nav>
   );
 }
