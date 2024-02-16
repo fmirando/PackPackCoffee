a82d02ea5a9bca5c69350da70d41367fd9b55ce2
Frank <mirandofrank@gmail.com>
2024-02-15 17:06:35 -0800

    Added routes to App.jsx

--- CLIENT/src/components/App.jsx
+++ CLIENT/src/components/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './Navbar';
+import Landing from './Landing/Landing';
+import About from './About/About';
+import Contact from './Contact/Contact';
+import Gallery from './Gallery/Gallery';
+import Packages from './PackagesMenu/Packages';
 
 export default function App() {
-  // TODO: Mount Navbar component below
   return (
     <div className="App">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/packages" element={<Packages />} />
+      </Routes>
     </div>
   );
 }
