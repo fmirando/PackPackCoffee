7c69af905a2e4441aa006426de9c088e51452f90
Frank <mirandofrank@gmail.com>
2024-02-19 18:08:34 -0800

    Created a simple navbar using bootstrap

--- CLIENT/src/components/Navbar.jsx
+++ CLIENT/src/components/Navbar.jsx
@@ -2,32 +2,33 @@
 // Navbar should contain PackPack logo/name, and the following tabs: About, Contact, Gallery, and Packages
 // Note that "Landing" component should not be included as a tab but rather the user can click on the name/logo and will be directed to landing page
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <nav>
-      <Link to="/" className="title">PackPack Coffee</Link>
-      <div>
-        <span></span>
-        <span></span>
-        <span></span>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <NavLink to="/" className="navbar-brand title">PackPack Coffee</NavLink>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <NavLink exact to="/about" className="nav-link" activeClassName="active">About</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink exact to="/contact" className="nav-link" activeClassName="active">Contact</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink exact to="/gallery" className="nav-link" activeClassName="active">Gallery</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink exact to="/packages" className="nav-link" activeClassName="active">Packages</NavLink>
+            </li>
+          </ul>
+        </div>
       </div>
-
-      <ul>
-        <li>
-          <NavLink to="/about">About</NavLink>
-        </li>
-        <li>
-          <NavLink to="/contact">Contact</NavLink>
-        </li>
-        <li>
-          <NavLink to="/gallery">Gallery</NavLink>
-        </li>
-        <li>
-          <NavLink to="/packages">Packages</NavLink>
-        </li>
-      </ul>
     </nav>
   );
 }
