34b99f5555acae4f9964f11050faee8aad372d99
Frank <mirandofrank@gmail.com>
2024-03-19 16:30:27 -0700

    Added "BOOK US TODAY!" button to navbar

--- CLIENT/src/components/NavBar/Navbar.jsx
+++ CLIENT/src/components/NavBar/Navbar.jsx
@@ -1,34 +1,44 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
-import styles from "./NavBar.modules.css";
+import styles from './NavBar.modules.css';
 
-
-const NavBar = () => {
+function NavBar() {
   return (
     <Navbar expand="lg" className={styles.navbar}>
-    <Container className={styles.navbarContainer}>
-      <Navbar.Brand as={NavLink} to="/">PackPack</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link as={NavLink} to="/about" className={styles.navLink}>
-            About
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/contact" className={styles.navLink}>
-            Contact
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/gallery" className={styles.navLink}>
-            Gallery
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/packages" className={styles.navLink}
-          >Packages
-          </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
+      <Container className={styles.navbarContainer}>
+        <Navbar.Brand as={NavLink} to="/">PackPack</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={NavLink} to="/about" className={styles.navLink}>
+              About
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/contact" className={styles.navLink}>
+              Contact
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/gallery" className={styles.navLink}>
+              Gallery
+            </Nav.Link>
+            <Nav.Link
+              as={NavLink}
+              to="/packages"
+              className={styles.navLink}
+            >
+              Packages
+            </Nav.Link>
+            <Nav.Link
+              as={NavLink}
+              to="/quotes"
+              className={styles.navLink}
+            >
+              BOOK US TODAY!
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
-};
+}
 
 export default NavBar;
