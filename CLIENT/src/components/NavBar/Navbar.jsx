ce0721a58a6dd3de46d476311921b4c1223ccbc9
Frank <mirandofrank@gmail.com>
2024-04-03 17:58:28 -0700

    Centered elements in navbar

--- CLIENT/src/components/NavBar/Navbar.jsx
+++ CLIENT/src/components/NavBar/Navbar.jsx
@@ -1,42 +1,23 @@
 import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import styles from './NavBar.modules.css';
 
 function NavBar() {
   return (
     <Navbar expand="lg" className={styles.navbar}>
-      <Container className={styles.navbarContainer}>
-        <Navbar.Brand as={NavLink} to="/">PackPack</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={NavLink} to="/about" className={styles.navLink}>
-              About
-            </Nav.Link>
-            <Nav.Link as={NavLink} to="/contact" className={styles.navLink}>
-              Contact
-            </Nav.Link>
-            <Nav.Link as={NavLink} to="/gallery" className={styles.navLink}>
-              Gallery
-            </Nav.Link>
-            <Nav.Link
-              as={NavLink}
-              to="/packages"
-              className={styles.navLink}
-            >
-              Packages
-            </Nav.Link>
-            <Nav.Link
-              as={NavLink}
-              to="/quotes"
-              className={styles.navLink}
-            >
-              BOOK US TODAY!
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+      <div className={styles.navbarContent}>
+        <Navbar.Brand as={NavLink} to="/" className={styles.navbarBrand}>PackPack</Navbar.Brand>
+        <Nav className={`${styles.navLink} ${styles.navbarLinks}`}>
+          <Nav.Link as={NavLink} to="/about">About</Nav.Link>
+          <Nav.Link as={NavLink} to="/contact">Contact</Nav.Link>
+          <Nav.Link as={NavLink} to="/gallery">Gallery</Nav.Link>
+          <Nav.Link as={NavLink} to="/packages">Packages</Nav.Link>
+        </Nav>
+        <Nav className={styles.navLink}>
+          <Nav.Link as={NavLink} to="/quotes">BOOK US TODAY!</Nav.Link>
+        </Nav>
+      </div>
     </Navbar>
   );
 }
