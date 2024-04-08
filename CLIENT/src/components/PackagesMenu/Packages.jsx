9e7c560520efccfb2b97608c87e62d93e2af5c02
Frank <mirandofrank@gmail.com>
2024-04-08 16:39:28 -0700

    Added package descriptions for each

--- CLIENT/src/components/PackagesMenu/Packages.jsx
+++ CLIENT/src/components/PackagesMenu/Packages.jsx
@@ -11,6 +11,16 @@ export default function Packages() {
       <p>Packages</p>
       <img src={SelfServeImg} alt="SelfServe" />
       <img src={BaristaPrepImg} alt="BaristaPrep" />
+      <p>
+        Self-Serve
+        What's included? We provide your choice of beverages in self-serve dispensers, cups, lids, straws, stirrers, sugar, and cream. Set-up is also provided and everything can be taken home or disposed of at the end of the day.
+        Packages start at $350
+      </p>
+      <p>
+        Barista-Prep
+        What's included? This package includes a coffee cart, and a barista at your even, providing handcrafted drinks to your guests. Live customization of drinks offer a larger menu and dietary accommodations.
+        Packages start at $410
+      </p>
     </div>
   );
 }
