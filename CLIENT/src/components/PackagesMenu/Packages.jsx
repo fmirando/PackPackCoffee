3ba1f365aef8b94b9a7ebb20e5c7d587c008aebe
Frank <mirandofrank@gmail.com>
2024-04-08 16:29:39 -0700

    Was able to import and render sample images on Packages page

--- CLIENT/src/components/PackagesMenu/Packages.jsx
+++ CLIENT/src/components/PackagesMenu/Packages.jsx
@@ -1,12 +1,16 @@
 // This component will display the different pacakges that Lauren offers
 // Will also contain a contact form for quotes
-
 import React from 'react';
+
+import SelfServeImg from '../../../public/images/self_serve.png';
+import BaristaPrepImg from '../../../public/images/barista_prep.png';
 
 export default function Packages() {
   return (
     <div>
       <p>Packages</p>
+      <img src={SelfServeImg} alt="SelfServe" />
+      <img src={BaristaPrepImg} alt="BaristaPrep" />
     </div>
   );
 }
